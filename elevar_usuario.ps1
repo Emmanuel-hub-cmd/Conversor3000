52e7d292b7b2c974207308ee6fe027d9fb449ca2
Emmanuel-hub-cmd <ecollins@soterguard.com>
2025-06-30 12:56:29 +0200

    Update elevar_usuario.ps1

--- elevar_usuario.ps1
+++ elevar_usuario.ps1
@@ -8,24 +8,24 @@ if (-not $usuario) {
     exit 1
 }
 
-Write-Host " ====================" -ForegroundColor Cyan
-Write-Host " || CONVERSOR.3000 ||" -ForegroundColor Cyan
-Write-Host " ====================" -ForegroundColor Cyan
+Write-Host "  ====================" -ForegroundColor Cyan
+Write-Host "  || CONVERSOR.3000 ||" -ForegroundColor Cyan
+Write-Host "  ====================" -ForegroundColor Cyan
 Write-Host "" -ForegroundColor Cyan
-Write-Host "Usuario a elevar: $usuario" -ForegroundColor Cyan
+Write-Host " Usuario a elevar: $usuario" -ForegroundColor Cyan
 
 # === Verificar si ya es administrador ===
 $grupo = net localgroup Administradores
 if ($grupo -match [regex]::Escape($usuario)) {
-    Write-Host "El usuario $usuario ya es administrador. (No se realiza accion)" -ForegroundColor Yellow
+    Write-Host " El usuario $usuario ya es administrador. (No se realiza accion)" -ForegroundColor Yellow
 } else {
     net localgroup Administradores "$usuario" /add
     if ($LASTEXITCODE -eq 0) {
-        Write-Host "Usuario $usuario promovido correctamente a Administrador." -ForegroundColor Green
+        Write-Host " Usuario $usuario promovido correctamente a Administrador." -ForegroundColor Green
     } else {
-        Write-Host "Error al intentar agregar al usuario al grupo Administradores." -ForegroundColor Red
+        Write-Host " Error al intentar agregar al usuario al grupo Administradores." -ForegroundColor Red
     }
 }
 
 Write-Host ""
-Read-Host -Prompt "Presiona Enter para cerrar"
+Read-Host -Prompt " Presiona Enter para cerrar"
