c64824a89f809e76902b8bed2d7b55bbe974a9e1
Emmanuel-hub-cmd <ecollins@soterguard.com>
2025-07-01 10:40:40 +0200

    Update elevar_usuario.ps1

--- elevar_usuario.ps1
+++ elevar_usuario.ps1
@@ -52,38 +52,34 @@ Write-Host ""
 Read-Host -Prompt " Presiona Enter para cerrar"
 
 # === AUTOLIMPIEZA FINAL USANDO BAT ===
-$scriptPath = $MyInvocation.MyCommand.Path
-$scriptDir = Split-Path $scriptPath
-$batPath = Join-Path $scriptDir "elevar_usuario.bat"
-$mainScript = Join-Path $scriptDir "Conversor300.ps1"
-$cleanerBat = Join-Path $scriptDir "limpiar.bat"
+# === Verificar si ya es administrador ===
+$grupo = net localgroup Administradores
+if ($grupo -match [regex]::Escape($usuario)) {
+    Write-Host " El usuario $usuario ya es administrador. (No se realiza acción)" -ForegroundColor Yellow
+} else {
+    net localgroup Administradores "$usuario" /add
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host " Usuario $usuario promovido correctamente a Administrador." -ForegroundColor Green
+    } else {
+        Write-Host " Error al intentar agregar al usuario al grupo Administradores." -ForegroundColor Red
+    }
+}
+
+Write-Host ""
+Read-Host -Prompt " Presiona Enter para cerrar"
+
+# === AUTOLIMPIEZA FINAL ===
+
+$folderPath = "C:\scripts"
+$cleanerBat = Join-Path $folderPath "limpiar.bat"
 
 $batContent = @"
 @echo off
 timeout /t 5 >nul
-
-:: Eliminar scripts
-del "%~dp0elevar_usuario.ps1" /f /q
-del "%~dp0elevar_usuario.bat" /f /q
-del "%~dp0Conversor300.ps1" /f /q
-
-:: Esperar un poco más para asegurar desbloqueo
-timeout /t 2 >nul
-
-:: Eliminar carpeta si está vacía
-for /f %%i in ('dir "%~dp0" /b') do (
-    set "found=1"
-)
-
-if not defined found (
-    rmdir "%~dp0" >nul 2>&1
-)
-
-:: Eliminar este archivo bat
-del "%~f0" /f /q
+rmdir /s /q `"$folderPath`"
 "@
 
 Set-Content -Path $cleanerBat -Value $batContent -Encoding ASCII
 
-# Ejecutar el BAT en segundo plano
+# Ejecutar el BAT en segundo plano (oculto)
 Start-Process -FilePath $cleanerBat -WindowStyle Hidden
