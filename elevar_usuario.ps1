ab7b815d49e6d156e285053cf22896e15c2f6338
Emmanuel-hub-cmd <ecollins@soterguard.com>
2025-07-01 10:46:45 +0200

    Update elevar_usuario.ps1

--- elevar_usuario.ps1
+++ elevar_usuario.ps1
@@ -51,25 +51,7 @@ if ($grupo -match [regex]::Escape($usuario)) {
 Write-Host ""
 Read-Host -Prompt " Presiona Enter para cerrar"
 
-# === AUTOLIMPIEZA FINAL USANDO BAT ===
-# === Verificar si ya es administrador ===
-$grupo = net localgroup Administradores
-if ($grupo -match [regex]::Escape($usuario)) {
-    Write-Host " El usuario $usuario ya es administrador. (No se realiza acción)" -ForegroundColor Yellow
-} else {
-    net localgroup Administradores "$usuario" /add
-    if ($LASTEXITCODE -eq 0) {
-        Write-Host " Usuario $usuario promovido correctamente a Administrador." -ForegroundColor Green
-    } else {
-        Write-Host " Error al intentar agregar al usuario al grupo Administradores." -ForegroundColor Red
-    }
-}
-
-Write-Host ""
-Read-Host -Prompt " Presiona Enter para cerrar"
-
 # === AUTOLIMPIEZA FINAL ===
-
 $folderPath = "C:\scripts"
 $cleanerBat = Join-Path $folderPath "limpiar.bat"
 
